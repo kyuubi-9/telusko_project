42c3732b55051630977aa8725293251155fa2ef9
kyuubi-9 <hv.himanshi.verma@gmail.com>
2020-06-12 12:59:12 +0530

    done complete

--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
-def register(request):
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+
+def register(request): 
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -21,6 +38,7 @@ def register(request):
                 user = User.objects.create_user(username = username,password = password1, email = email, first_name = first_name,last_name = last_name)
                 user.save()
                 print('user created')
+                return redirect ('login')
         else:
             messages.info(request,'passwords not matching')
             return redirect('register')
@@ -30,5 +48,10 @@ def register(request):
 
     else:
         return render(request,'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
     
